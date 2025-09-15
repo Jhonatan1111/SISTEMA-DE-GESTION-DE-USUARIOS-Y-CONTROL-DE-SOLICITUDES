f347e3d628967f51d07f4f893153d50d86671877
alvitronex <marioenriquealvarado12@gmail.com>
2025-09-15 10:36:40 -0600

    SGUYCS-35 finalizado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,25 +16,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'nombre' => 'empleado',
-            'apellido' => 'empleado',
-            'email' => 'empleado@example.com',
-            'password' => '123456',
-            'role' => 'empleado',
-            'celular' => '70126656',
-            'email_verified_at' => now(),
-        ]);
+        User::firstOrCreate(
+            ['email' => 'empleado@example.com'],
+            [
+                'nombre' => 'Empleado',
+                'apellido' => 'Empleado',
+                'password' => bcrypt('123456'),
+                'role' => 'empleado',
+                'celular' => '70126656',
+                'email_verified_at' => now(),
+            ]
+        );
         // Crear un usuario administrador
-          User::factory()->create([
-            'nombre' => 'Admin',
-            'apellido' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => '123456',
-            'role' => 'admin',
-            'celular' => '73664989',
-            'email_verified_at' => now(),
-        ]);
+          User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'nombre' => 'Admin',
+                'apellido' => 'Admin',
+                'password' => bcrypt('123456'),
+                'role' => 'admin',
+                'celular' => '73664989',
+                'email_verified_at' => now(),
+            ]
+        );
     } 
 
     
