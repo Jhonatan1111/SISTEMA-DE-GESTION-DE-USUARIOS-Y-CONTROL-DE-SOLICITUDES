9fcefd5036482869ee32722cf3aa56f1d3c2ff81
Alvarokast <alvaro.cornejo0293@gmail.com>
2025-09-21 12:00:37 -0600

    SGUYCS-34 terminando login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,11 +50,12 @@
                 <label for="email" class="form-label">Correo electrónico</label>
                 <input type="email" name="email" id="email" class="form-control" required autofocus value="{{ old('email') }}">
             </div>
-            
+
             <div class="mb-3">
                 <label for="password" class="form-label">Contraseña</label>
                 <input type="password" name="password" id="password" class="form-control" required>
             </div>
+            
             <div class="mb-3 form-check">
                 <input type="checkbox" name="remember" id="remember" class="form-check-input">
                 <label class="form-check-label" for="remember">Recordarme</label>
